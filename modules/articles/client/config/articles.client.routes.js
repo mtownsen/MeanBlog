8801c765829f0ba54de678a96f2e2c0ae33becd0
Matthew Townsen <matthewtownsen@gmail.com>
2016-10-21 19:13:49 -0600

    update base route

--- modules/articles/client/config/articles.client.routes.js
+++ modules/articles/client/config/articles.client.routes.js
@@ -11,7 +11,7 @@
     $stateProvider
       .state('articles', {
         abstract: true,
-        url: '/articles',
+        url: '/',
         template: '<ui-view/>'
       })
       .state('articles.list', {
